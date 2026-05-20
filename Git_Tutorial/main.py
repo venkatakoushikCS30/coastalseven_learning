cedde6c44bde083aaef3770c4716bdbb19f838d3
venkata koushik <venkatakoushikCS30-G6@coastalseven.com>
2026-05-20 16:25:00 +0530

    Merged Git_Tutorial repository into subfolder

---
+++ Git_Tutorial/main.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("\n--- User Details ---")
+print("Name:", name)
+print("Age:", age)
+
+# Simple condition
+if age >= 18:
+    print("You are eligible to vote in india.")
+else:
+    print("You are not eligible to vote yet in india.")
+
+print("added from branch ")
+
+a=3/0.5
+print(a)
